7e56cbbabec7c94cb1baf95efc9dcd7c680195fb
Sanjib Kumar Deka <sanjibkumardeka14@gmail.com>
2025-05-06 00:06:09 +0530

    Update page.tsx

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,21 +1,22 @@
-"use client"
+'use client'
 
+import React from 'react'
 import ProjectCard from '@/components/ProjectCard'
 import { Projects } from '@/constants'
-import React from 'react'
 
 const Page = () => {
   return (
     <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
-     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
-      <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
+      className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
+      style={{ backgroundImage: 'url("/mountains.jpg")' }}
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-[90%] max-h-[90%] overflow-y-auto p-4">
         {Projects.map((project, index) => (
           <ProjectCard
-              key={index}
-              title={project.title}
-              text={project.text}
-              image={project.src}
+            key={index}
+            title={project.title}
+            text={project.text}
+            image={project.src}
           />
         ))}
       </div>
